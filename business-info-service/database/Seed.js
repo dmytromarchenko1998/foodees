adcd42dbb372e5ac4293271cf217ac11374ca371
Dmytro Marchenko <dmytromarchenko1998@gmail.com>
2018-04-28 11:12:54 -0700

    wcwecvwe

--- business-info-service/database/Seed.js
+++ business-info-service/database/Seed.js
@@ -3,9 +3,11 @@ mongoose.Promise = global.Promise;
 const restaurants = require('./las-vegas-restaurants.js').data;
 const db = require('./info.js').db;
 const Business = require('./info.js').dbHelpers;
-var query = Business.drop();
-query.exec(() => {console.log('cleared database')});
 var query = Business.find();
-Business.create(restaurants)
+var seed = () => {
+  query.exec(() => {console.log('cleared database')});
+  var business = Business.create(restaurants)
   .then(()=> query.exec((err, businesses) => {console.log(businesses.length)}))
   .then(()=> db.disconnect())
+}
+Business.remove({}, seed);
