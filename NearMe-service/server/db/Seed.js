722992ac736a0ea02b6526210ab76aa43fc4176a
Dmytro Marchenko <dmytromarchenko1998@gmail.com>
2018-04-28 11:06:47 -0700

    wcwec

--- NearMe-service/server/db/Seed.js
+++ NearMe-service/server/db/Seed.js
@@ -3,10 +3,11 @@ mongoose.Promise = global.Promise;
 const restaurants = require('./las-vegas-restaurants.js');
 const db = require('./Business.js').db;
 const Business = require('./Business.js').Business;
-var query = Business.drop();
-query.exec(() => {console.log('cleared database')});
 var query = Business.find();
-var business = Business.create(restaurants)
+var seed = () => {
+  query.exec(() => {console.log('cleared database')});
+  var business = Business.create(restaurants)
   .then(()=> query.exec((err, businesses) => {console.log(businesses.length)}))
   .then(()=> db.disconnect())
-// console.log(restaurants)
+}
+Business.remove({}, seed);
